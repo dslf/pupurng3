1b2f90fb84b9f0f4290d918af3e35cffe4118923
Denis Lafin <dslf@live.ru>
2025-02-21 00:54:03 +0300

    changed, need fixes

--- inturfjd.c
+++ inturfjd.c
@@ -4,31 +4,102 @@
 #include <stdlib.h>
 
 #define LENGTH 5
-#define COUNT 2000
+#define COUNT 120
+#define MAX_CHAR_REPEAT 4
+
+int array_isfull(int *arr, int value_max, int size) 
+{
+	for (int i = 0; i < size; i++) 
+		if (arr[i] < value_max) {
+		//	printf("\nfalse %d:%d\n", i, arr[i]);
+			return 0;
+		} 		
+		//	printf("\ntrue\n");
+			return 1;		
+}
+
+int array_sum(int *arr, int size) 
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+		sum+=arr[i];
+	return sum;
+
+}
 
 int main() {
 
-    char char_array[] = "abcdefghijklmnopqrstuvwxyz";
-    int char_count[26] = {0};
-    int len = strlen(char_array), char_random, char_prev_random = 42;
-    
-    srand(time(NULL));
+	char char_array[] = "abcdefghijklmnopqrstuvwxyz";
+	int char_count[27] = {0};
+	int len = strlen(char_array), char_random, char_prev_random = 42;
+	int count_len_chars = 0, count_len_words = 0;
+	
+	srand(time(NULL));
 
-    for (int i = 0; i < COUNT; i++) {
-        for (int k = 0; k < LENGTH; k++) {
-            char_random = rand() % len;
-            while (char_random == char_prev_random) 
-                char_random = rand() % len;
-            putchar(char_array[char_random]);
-            char_count[char_random]++;
-            char_prev_random = char_random;
-        }
-        printf(" ");
-    }
+	while(1) {
+		char_random = rand() % len;
+		
+		if (count_len_chars >= LENGTH) {
+			count_len_chars = 0;
+			count_len_words++;
+			printf(" ");
+		}
 
-    printf("\n\n--------\nchar(count): ");
-    for (int i = 0; i < len; i++)        
-            printf("%c(%d) ", char_array[i], char_count[i]);
-    printf("\n\nused chars: %s (len: %d)\n", char_array, len);
-    return 0;
+		if (count_len_words >= COUNT) break;
+
+		if (array_isfull(char_count, MAX_CHAR_REPEAT, len)) {
+			printf("\nfull/break1\n"); 
+			break; 
+		}
+
+		while (char_random == char_prev_random || char_count[char_random] > MAX_CHAR_REPEAT) {
+			if (array_isfull(char_count, MAX_CHAR_REPEAT, len)) {
+				printf("full/break\n"); 
+				break; 
+			}
+			char_random = rand() % len;
+		//	printf("random char = %d\n", char_random);
+		}
+
+		//printf("random double? char = %d\n", char_random);
+
+		putchar(char_array[char_random]);
+		char_count[char_random]++;
+		char_prev_random = char_random;
+		count_len_chars++;
+
+		//break;
+	}
+	
+/* 	for (int i = 0; i < 0; i++) {
+		if (array_isfull(char_count, 2, len)) break;
+		for (int k = 0; k < LENGTH; k++) {
+			if (array_isfull(char_count, 2, len)) break;
+			char_random = rand() % len;
+
+			while ((char_count[char_random] > COUNT - 1) || (char_random == char_prev_random))
+			{
+				if (array_sum(char_count, len) > LENGTH * COUNT)
+					break;
+				char_random = rand() % len;
+				// printf("debug %d\n", array_sum(char_count, len));
+			}
+
+			while (char_random == char_prev_random) 
+				char_random = rand() % len; 
+
+			putchar(char_array[char_random]);
+			char_count[char_random]++;
+			char_prev_random = char_random;
+			//printf("%d ", array_isfull(char_count, 1, len));
+		}
+		printf(" ");
+	} */
+
+	printf("\n\n--------\nchar(count): ");
+	for (int i = 0; i < len; i++)        
+			printf("%c(%d) ", char_array[i], char_count[i]);
+	printf("\n\nused chars: %s (len: %d)\n", char_array, len);
+	printf("%d \n", array_sum(char_count, len));
+	return 0;
 }
